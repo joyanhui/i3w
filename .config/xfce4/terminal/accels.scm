5688b3ebf8c0f4db43e2baba9d8d1a305c0d95dd
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-04-26 19:21:56 +0800

    Create accels.scm

---
+++ .config/xfce4/terminal/accels.scm
@@ -0,0 +1,4 @@
+; (gtk_accel_path "<Actions>/terminal-window/close-tab" "<Primary><Shift>w")
+(gtk_accel_path "<Actions>/terminal-window/close-tab" "<Primary>w")
+; (gtk_accel_path "<Actions>/terminal-window/new-tab" "<Primary><Shift>t")
+(gtk_accel_path "<Actions>/terminal-window/new-tab" "<Primary>t"
