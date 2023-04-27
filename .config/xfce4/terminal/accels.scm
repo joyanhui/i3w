9ca34481acb64ec25ea28915323c50ae5d18187f
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-04-27 16:53:29 +0800

    Delete accels.scm

--- .config/xfce4/terminal/accels.scm
+++
@@ -1,4 +0,0 @@
-; (gtk_accel_path "<Actions>/terminal-window/close-tab" "<Primary><Shift>w")
-(gtk_accel_path "<Actions>/terminal-window/close-tab" "<Primary>w")
-; (gtk_accel_path "<Actions>/terminal-window/new-tab" "<Primary><Shift>t")
-(gtk_accel_path "<Actions>/terminal-window/new-tab" "<Primary>t"
